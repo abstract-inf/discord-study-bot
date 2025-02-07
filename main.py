a35790b4b9b4f87c650d250d1e75468b85328808
Yazan-1st <yazanatout1@gmail.com>
2025-02-08 00:08:15 +0300

    added logging system using csv files

--- main.py
+++ main.py
@@ -17,6 +17,21 @@ from dotenv import load_dotenv
 import datetime
 import asyncio
 import messages_handler
+import pandas as pd
+
+# Create a DataFrame to store logs
+studyLogsDF = pd.DataFrame(columns=['Name', 'Start Time', 'End Time', 'Time Spent'])
+# Create a DataFrame to store channel logs
+channelLogsDF = pd.DataFrame(columns=['Channel','User', 'Time', 'isJoin'])  #isJoin is a boolean to indicate if the user joined or left the channel
+
+# Use existing CSV logs file if it exists
+if os.path.exists('study_logs.csv'):
+    studyLogsDF = pd.read_csv('study_logs.csv')
+    channelLogsDF = pd.read_csv('channel_logs.csv')
+# Create a new CSV logs file if it doesn't exist
+else:
+    studyLogsDF.to_csv('study_logs.csv', index=False)
+    channelLogsDF.to_csv('channel_logs.csv', index=False)
 
 # Load environment variables
 load_dotenv()
@@ -80,6 +95,8 @@ async def on_ready():
 # this function also handles the case where a user wants to START or STOP studying
 @client.event
 async def on_message(message):
+    global studyLogsDF
+    # this is not to confuse the bot with its own messages
     if message.author == client.user:
         return
 
@@ -90,7 +107,7 @@ async def on_message(message):
     # this is supposed to handle the case where the user wants to start studying
     if message.content.lower() in ['!study', '!start']:
         if message.author.name in is_studying and is_studying[message.author.name]:
-            await message.channel.send(f"📚 @<@{message.author.id}> is already studying.")
+            await message.channel.send(f"📚 <@{message.author.id}> is already studying.")
         else:
             is_studying[message.author.name] = True  # set the user as studying
             await message.channel.send(f"📚 <@{message.author.id}> has started studying.")
@@ -124,12 +141,20 @@ async def on_message(message):
             print(f"{GREEN}{message.author.name} has stopped studying{RESET}")
 
             # log the time the user stopped studying
-            total_time = datetime.datetime.now() - study_start_times.pop(message.author.name)
+            start_time = study_start_times.pop(message.author.name)
+            total_time = datetime.datetime.now() - start_time
 
             # format the time the user spent studying hours, minutes, and seconds
             hours, remainder = divmod(int(total_time.total_seconds()), 3600)
             minutes, seconds = divmod(remainder, 60)
             formatted_time = f"{hours:02}:{minutes:02}:{seconds:02}"
+
+            # log the time the user spent studying in CSV file
+            # this is to keep track of the time the user spent studying
+            studyLogsDF.loc[len(studyLogsDF)] = {'Name': message.author.name, 'Start Time': start_time, 'End Time': datetime.datetime.now(), 'Time Spent': total_time}
+            studyLogsDF.to_csv('study_logs.csv', index=False)
+            print(f"{GREEN}Logged studying time for {message.author.name}: {studyLogsDF.loc[len(studyLogsDF)-1]}{RESET}")
+
 
             # create an embed to log the time the user stopped studying
             # this is simply a visual representation of the time the user stopped studying
@@ -151,7 +176,7 @@ async def on_message(message):
     
     else:
         msg = message.content.lower()
-        print(f"{message.channel}{BLUE}Received message from {PURPLE}[{message.author}]:{RESET} {msg}")
+        print(f"[{message.channel}] {BLUE}Received message from {PURPLE}[{message.author}]:{RESET} {msg}")
 
         bot_response = messages_handler.find_response(msg)
         if bot_response:
@@ -165,7 +190,7 @@ async def on_message(message):
         
 
 # this was supposed to handle the case where the user joins or leaves a voice channel
-'''
+# now this function logs the time the user joins or leaves a voice channel
 @client.event
 async def on_voice_state_update(member, before, after):
     if before.channel != after.channel:
@@ -174,36 +199,39 @@ async def on_voice_state_update(member, before, after):
 
         # User joins a voice channel
         if before.channel is None and after.channel is not None:
-            study_start_times[member.name] = datetime.datetime.now()
-            if text_channel:
-                embed = discord.Embed(
-                    title=f"**@{member.name} Started Studying**",
-                    color=discord.Color.blue()
-                ).add_field(
-                    name='Time started Studying',
-                    value=f"**```{datetime.datetime.now().strftime('%I:%M:%S %p')}```**"
-                )
-                await text_channel.send(embed=embed)
-                print(f"{GREEN}Sent {YELLOW}studying start {GREEN}log for {PURPLE}{member.name} {RESET}[{datetime.datetime.now().strftime('%I:%M:%S %p')}]")
+            channelLogsDF.loc[len(channelLogsDF)] = {'Channel': after.channel.name, 'User': member.name, 'Time': datetime.datetime.now(), 'isJoin': True}
+            channelLogsDF.to_csv('channel_logs.csv', index=False)
+            # study_start_times[member.name] = datetime.datetime.now()
+            # if text_channel:
+                # embed = discord.Embed(
+                #     title=f"**@{member.name} Started Studying**",
+                #     color=discord.Color.blue()
+                # ).add_field(
+                #     name='Time started Studying',
+                #     value=f"**```{datetime.datetime.now().strftime('%I:%M:%S %p')}```**"
+                # )
+                # await text_channel.send(embed=embed)
+                # print(f"{GREEN}Sent {YELLOW}studying start {GREEN}log for {PURPLE}{member.name} {RESET}[{datetime.datetime.now().strftime('%I:%M:%S %p')}]")
 
         # User leaves a voice channel
         elif before.channel is not None and after.channel is None:
-            if member.name in study_start_times and text_channel:
-                total_time = datetime.datetime.now() - study_start_times.pop(member.name)
-                hours, remainder = divmod(int(total_time.total_seconds()), 3600)
-                minutes, seconds = divmod(remainder, 60)
-                formatted_time = f"{hours:02}:{minutes:02}:{seconds:02}"
+            # if member.name in study_start_times and text_channel:
+            channelLogsDF.loc[len(channelLogsDF)] = {'Channel': before.channel.name, 'User': member.name, 'Time': datetime.datetime.now(), 'isJoin': False}
+            channelLogsDF.to_csv('channel_logs.csv', index=False)
+                # total_time = datetime.datetime.now() - study_start_times.pop(member.name)
+                # hours, remainder = divmod(int(total_time.total_seconds()), 3600)
+                # minutes, seconds = divmod(remainder, 60)
+                # formatted_time = f"{hours:02}:{minutes:02}:{seconds:02}"
 
-                embed = discord.Embed(
-                    title=f"**Total Time @{member.name} Spent Studying**",
-                    color=discord.Color.blue()
-                ).add_field(
-                    name='Time Spent Studying',
-                    value=f"**```{formatted_time}```**"
-                ).set_image(url=get_appropriate_gif(hours, minutes))
-                await text_channel.send(embed=embed)
-                print(f"{GREEN}Sent studying time log for {member.name}: {formatted_time}{RESET}")
-'''
+                # embed = discord.Embed(
+                #     title=f"**Total Time @{member.name} Spent Studying**",
+                #     color=discord.Color.blue()
+                # ).add_field(
+                #     name='Time Spent Studying',
+                #     value=f"**```{formatted_time}```**"
+                # ).set_image(url=get_appropriate_gif(hours, minutes))
+                # await text_channel.send(embed=embed)
+                # print(f"{GREEN}Sent studying time log for {member.name}: {formatted_time}{RESET}")
 
 def get_appropriate_gif(hours, minutes):
     # this dictionary contains the range of hours spent studying and the corresponding gif
